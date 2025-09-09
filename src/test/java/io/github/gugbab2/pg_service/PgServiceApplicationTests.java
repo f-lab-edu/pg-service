54779276a6b9d557ddddf1d29cad69b340c459ec
gugbab2 <jkhoo1997@gmail.com>
2025-09-10 07:47:03 +0900

    Initial commit with project setup

---
+++ src/test/java/io/github/gugbab2/pg_service/PgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gugbab2.pg_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
